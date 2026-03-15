ccdd2ec1c0bfb2e764443d6a48f3c482febf65cf
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2026-03-15 13:36:52 -0300

    update: atualiza o LoginFragment com campos para exibir / esconder senha

--- app/src/main/java/br/com/denisecastro/taskapp/ui/auth/LoginFragment.kt
+++ app/src/main/java/br/com/denisecastro/taskapp/ui/auth/LoginFragment.kt
@@ -1,18 +1,26 @@
 package br.com.denisecastro.taskapp.ui.auth
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.method.PasswordTransformationMethod
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import br.com.denisecastro.taskapp.R
 import br.com.denisecastro.taskapp.databinding.FragmentLoginBinding
-import br.com.denisecastro.taskapp.databinding.FragmentSplashBinding
+import br.com.denisecastro.taskapp.navigation.navToRecover
+import br.com.denisecastro.taskapp.navigation.navToRegister
+import br.com.denisecastro.taskapp.ui.viewmodel.LoginViewModel
 
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: LoginViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,12 +28,49 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initClicks()
+        setupPasswordVisibility()
+    }
+    private fun initClicks() {
+        binding.buttonLogin.setOnClickListener {
+            binding.editTextEmail.text.toString()
+            binding.editTextPassword.text.toString()
+        }
+        binding.textCreateAccount.setOnClickListener {
+            navToRegister()
+        }
+        binding.textRecoverAccount.setOnClickListener {
+            navToRecover()
+        }
+    }
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setupPasswordVisibility() {
+        binding.editTextPassword.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                val drawableRight = 2
+                val iconWidth = binding.editTextPassword.compoundDrawables[drawableRight].bounds.width()
+                if (event.rawX >= (binding.editTextPassword.right - iconWidth - 50)) {
+                    val selection = binding.editTextPassword.selectionEnd
+                    val isPasswordVisible = binding.editTextPassword.transformationMethod == null
+
+                    if (isPasswordVisible) {
+                        binding.editTextPassword.transformationMethod = PasswordTransformationMethod()
+                        binding.editTextPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_eye, 0)
+                    } else {
+                        binding.editTextPassword.transformationMethod = null
+                        binding.editTextPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_eye_off, 0)
+                    }
+                    binding.editTextPassword.setSelection(selection)
+                    return@setOnTouchListener true
+                }
+            }
+            false
+        }
     }
 
     override fun onDestroyView() {

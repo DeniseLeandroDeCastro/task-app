203f5f45bef8650d2dcdbbcf937acf57912276b0
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2026-03-15 11:03:36 -0300

    feature: adiciona viewBinding ao Fragment Splash

--- app/src/main/java/br/com/denisecastro/taskapp/ui/splash/SplashFragment.kt
+++ app/src/main/java/br/com/denisecastro/taskapp/ui/splash/SplashFragment.kt
@@ -1,18 +1,43 @@
 package br.com.denisecastro.taskapp.ui.splash
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import br.com.denisecastro.taskapp.R
+import br.com.denisecastro.taskapp.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
 
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+    ): View {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed(this::checkAuth, 3000)
+    }
+
+    private fun checkAuth() {
+        findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
